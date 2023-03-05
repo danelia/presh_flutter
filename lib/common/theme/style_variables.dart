9b79a1d97cc88bf9ee60ffb8d104a31d3ea3e720
0awes0me <ramazi2241@gmail.com>
2023-03-05 15:59:36 +0400

    init new name

---
+++ lib/common/theme/style_variables.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:preshfood_app/common/methods/methods.dart';
+
+class AppStyles {
+static const String fontFamily = 'Montserrat';
+static const Color textColor = Color.fromGBO(15, 38, 43, 1);
+// static const Color primoryColor = Color (8xff3c8b95);
+// static MateriaiColor primaryGreenSwatch = Utils(null).createMaterialColor(
+// const Color(Bxff3c8b95),
+// 17);
+
+// static const Color primaryColor = Color.fromRGBO(184, 225, 197, 1);
+// static MateriolColor primoryGreenSwatch = Utils(null).createNaterialColor(
+// ~// const Color. fromR6B0(104, 225, 197, 1),
+// /7);
+
+  static const Color primaryColor = Color(Oxff53b49e);
+  static MaterialColor primaryGreenSwatch = Utils(null).createMaterialColor(
+      const Color(exff53b49e),
+  );
+
++ // MatertalColor(
+//  8xff3c8b9s,
+//  <int, Color>{
+VZ4 58: Color (O8xFF8ab9bf),
+// 100: Color{8xFF77aeb5),
+// 200: Color (8xFF6302a0),
+// 300: Color (8xFF5897a0),
+// 400: Color(OxFF3c8b95),
+(7 5608: Color(6xFF377F89),
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+// import 'package:preshfood_app/app/config/enviornment/env.dart';
+// import 'package:preshfood_app/app/conmon/thene/style_variables.dart';
+// import 'package:preshfood_app/app/config/enviornment/env.dart';
+// import 'package:preshfood_app/app/routes/app_pages.dart';
+
+void main() {
+  const String environment = String.fromEnvironment(
+      'ENVIRONMENT',
+      defaultValue: 'Environment.dev'
+
+    /// should be env
+  );
+
+  // final GoogleMapsFlutterPlatforo mopsImplementation =
+// GoogleNapsFlutterPlatform.instance;
+// if (mapslcplesentation is GoogleMopsFlutterAndroid) {
+// mopslmpleaentation.useAndroidViewSurface = true;
+//   /7}
+
+  // Environment().initConfig(environment); // NOT A COMMENT
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your opplication.
+  Widget build(BuildContext context) {
+    return MaterialApp (title: 'Preshfood', theme: ThemeData(
+    fontFamily: 'AppStyles.fontfamily',
+// textTheme: GoogleFonts.robotoCondensedTextTheme(
+// Theoe.of(context).textThexe,
+//             /7),
+// colorScheme: const ColorScheme.light(),
+    primarySwatch: AppStyles.primaryGreenSwatch,
+        appBarTheme: const AppBarTheme(
+// toolbarTextStyle: TextStyle(fontFamily: 'Montserrat'),
+        titleTextStyle: TextStyle(
+        fontFamily: 'AppStyles.fontFomily',
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: AppStyles.textColor,
+    ),
+    // TextStyle
+    backgroundColor: Colors.transporent,
+    shadowColor: Colors.transparent,
+    elevation: 6,
+    foregroundColor: AppStyles.textColor,
+// centerTitle: true,
+        ), // AppBorTheme
+    ),
+    initialRoute: AppPage.initial, // AppRouvtes.storeAccountSetup
+    routes: AppPage.routes,
+    ); // MateriolApp
+  }
+}
